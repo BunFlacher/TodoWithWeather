12f8c1b5ee9cb72b414cf44663dfe29a482b8cf2
Roman Kuznetsov <romakuznetsov832@gmail.com>
2023-06-08 12:37:55 +0300

    fix add task

--- src/Todo/TodoList.tsx
+++ src/Todo/TodoList.tsx
@@ -3,19 +3,22 @@ import { TodoItem } from "./TodoModel";
 
 const TodoList = () => {
     const [todos, setTodos] = useState<TodoItem[]>([])
+    const [text, setText] = useState<string>("")
 
     const addTodo = (text: string) => {
+        if(text.trim() === "") return 0
         const newTodo = { id: todos.length + 1, text, completed: false }
         setTodos([...todos, newTodo])
+        setText("")
     }
     const removeTodo = (id: number) => {
         setTodos(todos.filter(todo => todo.id !== id))
     }
     const toggleComplete = (id: number) => {
         setTodos(
-            todos.map(todo => 
-                    todo.id === id ? { ...todo, completed: !todo.completed } : todo
-                )
+            todos.map(todo =>
+                todo.id === id ? { ...todo, completed: !todo.completed } : todo
+            )
         )
     }
 
@@ -24,25 +27,24 @@ const TodoList = () => {
             <ul>
                 {todos.map(todo => (
                     <li key={todo.id}>
-                        <input 
-                            type="text" 
+                        <input
+                            type="text"
                             checked={todo.completed}
                             onChange={() => toggleComplete(todo.id)}
-                            />
-                            {todo.text}
-                            <button onClick={() => removeTodo(todo.id)}>X</button>
+                        />
+                        {todo.text}
+                        <button onClick={() => removeTodo(todo.id)}>X</button>
                     </li>
                 ))}
             </ul>
-            <form onSubmit={e => {
-                e.preventDefault()
-                const todo = e.target.todo.value
-                addTodo(todo);
-                ((e.target) as HTMLFormElement).reset()
-            }}>
-            <input type="text" name="todo" placeholder="Enter a task" />
-            <button>Add Task</button>
-            </form>
+
+            <input type="text"
+                name="todo"
+                placeholder="Enter a task" value={text}
+                onChange={(e) => { setText(e.currentTarget.value) }}
+            />
+            <button onClick={() => { addTodo(text) }}>Add Task</button>
+
         </div>
     )
 }
